bdd58f4319b20817c4b51e1ecd1808be73581706
Aaromxj <49143314+Aaromxj@users.noreply.github.com>
2021-11-03 10:01:14 +0100

    Update __init__.py

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -9,5 +9,3 @@ from __future__ import division
 from __future__ import print_function
 
 from .cityscapes import Cityscapes as cityscapes
-from .lip import LIP as lip
-from .pascal_ctx import PASCALContext as pascal_ctx
